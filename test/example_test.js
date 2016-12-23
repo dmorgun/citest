42588d9671aab451e254d8b293e56b4340c7bff2
dmorgun <daryamorgun@gmail.com>
2016-12-23 21:04:20 +0200

    Update example_test.js

--- test/example_test.js
+++ test/example_test.js
@@ -3,9 +3,9 @@ var assert = require('assert')
 var should = require('should');
 
 var branchName = process.env.TRAVIS_BRANCH
-var branchUrl = branchName
-     ? 'http://dmorgun-' + branchName + '.herokuapp.com'
-     : 'http://myurl.com'
+     var branchUrl = branchName
+          ? 'http://dmorgun-' + branchName + '.herokuapp.com'
+          : 'http://myurl.com'
     
 describe('Home page tests', function() {
     it('Header is correct', function() {
